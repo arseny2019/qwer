ea321e548712571bf94549c885bce7d3e77678ba
arseny2019 <mrpeaklz@yandex.ru>
2019-06-05 21:00:30 +0400

    seccommit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        speed: 1000,
+        prevArrow: $('.slider__arrow_prev'),
+        nextArrow: $('.slider__arrow_next')
+    });
+
+
+});
